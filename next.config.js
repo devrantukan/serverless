267f8503a5992cbaabf5f36d1721d1d5b0199bab
devrantukan <devrantukan@gmail.com>
2023-10-11 00:30:03 +0300

    tailwind setup done

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config.resolve = {
+            ...config.resolve,
+            fallback: {
+              net: false,
+              dns: false,
+              tls: false,
+              fs: false,
+              request: false,
+            },
+          };
+        }
+        return config;
+      },
+  };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,20 +2,33 @@ import useSWR from 'swr'
 import PersonComponent from '../components/Person'
 import type { Person } from '../interfaces'
 
-const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
-export default function Index() {
-  const { data, error, isLoading } = useSWR<Person[]>('/api/people', fetcher)
 
-  if (error) return <div>Failed to load</div>
-  if (isLoading) return <div>Loading...</div>
-  if (!data) return null
+export default function Index(rates) {
+  
+
+  console.log(rates)
 
   return (
-    <ul>
-      {data.map((p) => (
-        <PersonComponent key={p.id} person={p} />
-      ))}
-    </ul>
+    <div >
+    <h1 className="text-2xl font-bold underline">{rates.rates[0]} dönemine ait enflasyon verileri</h1>
+      <p>%{rates.rates[1]} Yıllık</p>
+      <p>%{rates.rates[2]} Aylık</p>
+    </div>
   )
+}
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const res = await fetch('http://localhost:3000/inflation.json')
+  const rates = await res.json()
+ //console.log(rates)
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      rates,
+    },
+  }
 }
